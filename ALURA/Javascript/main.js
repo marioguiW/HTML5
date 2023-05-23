67a521ca0d65b5bdd28ad34a1c9510017c9a8fe7
marioguiW <126964100+marioguiW@users.noreply.github.com>
2023-05-23 17:32:27 -0300

    23-05

--- ALURA/Javascript/main.js
+++ ALURA/Javascript/main.js
@@ -1,18 +1,18 @@
-
-function tocaSomPom() {
-document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
-
-    contador = contador + 1;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
 }
-  
-
